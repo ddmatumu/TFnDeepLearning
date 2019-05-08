2f28edd7c39196fcf9d7788867348d8fd45b9713
Son Vu <sonvx@cs.umu.se>
2019-05-08 14:55:38 +0200

    Fixed long file names

--- codes/jupyterlabs/day1/scripts/03.run_etnlp_visualizer_inter_local.sh
+++ codes/jupyterlabs/day1/scripts/03.run_etnlp_visualizer_inter_local.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../../../pythonlibs/embeddings/etnlp/src/codes/
 export PYTHONPATH="$PYTHONPATH:$PWD"
-INPUT_FILES="../../../../../../data/etnlp/Tiny_fasttext_wiki_subword.vec;../../../../../../data/etnlp/Tiny_fasttext_wiki.vec;../../../../../../data/etnlp/Tiny_glove.840B.300d.vec;../../../../../../data/etnlp/Tiny_W2V_google_news.vec"
+INPUT_FILES="../../../../../../data/etnlp/Ti_ft_wi_sub.vec;../../../../../../data/etnlp/Ti_ft_wi.vec;../../../../../../data/etnlp/Ti_gl.vec;../../../../../../data/etnlp/Ti_W2V.vec"
 python3 ./etnlp_api.py  -input $INPUT_FILES -args visualizer -port 8891
